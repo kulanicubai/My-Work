128299b6ae4cc14928643ce0f6282f1048a8d16a
kulanicubai <kulpro26cubai@gmail.com>
2024-07-23 04:32:03 -0700

    Add files via upload
    working on home page

---
+++ New folder/links.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head> 
+<title> about us<title> 
+<head> 
+<h1> about us</h1>
+<p><a
+ref="https://www.about us.com/">about.com</a</p>
+<h2>contact us</h2>
+<p><a
+ref="https://www.contact us.com/">contact us.com</a<p>
+</body>
+</html> 
